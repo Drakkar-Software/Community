6031f0a261e1c4a65199ac0ad8d6837c26d1f9f6
herklos <herklos.dev@protonmail.com>
2021-02-07 23:04:15 +0100

    [Migrations] Fix spree backtesting migrations with unknown classe

--- db/migrate/20200905222212_add_microtransaction_credits_to_spree_users.spree_credits.rb
+++ db/migrate/20200905222212_add_microtransaction_credits_to_spree_users.spree_credits.rb
@@ -1,7 +1,7 @@
 # This migration comes from spree_credits (originally 20190727144236)
 class AddMicrotransactionCreditsToSpreeUsers < ActiveRecord::Migration[4.2]
   def change
-    unless Spree::User.column_names.include? "microtransaction_credits"
+    unless column_exists? :spree_users, :microtransaction_credits
       add_column :spree_users, :microtransaction_credits, :decimal, precision: 15, scale: 2
     end
   end
--- db/migrate/20200905222540_add_token_to_spree_backtesting_runs.spree_backtesting.rb
+++ db/migrate/20200905222540_add_token_to_spree_backtesting_runs.spree_backtesting.rb
@@ -1,7 +1,7 @@
 # This migration comes from spree_backtesting (originally 20190924205451)
 class AddTokenToSpreeBacktestingRuns < ActiveRecord::Migration[5.2]
   def change
-    unless Spree::BacktestingRun.column_names.include? "token"
+    unless column_exists? :spree_backtesting_runs, :token
       add_column :spree_backtesting_runs, :token, :string
     end
     add_index :spree_backtesting_runs, :token
