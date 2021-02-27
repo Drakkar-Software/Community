0f45584c6e7d8571b3a53a476fea1d396d742c64
GuillaumeDSM <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-02-27 23:40:21 +0100

    add sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,58 @@
+if Spree::Store.default.url.include? "https://"
+  SitemapGenerator::Sitemap.default_host = Spree::Store.default.url
+else
+  SitemapGenerator::Sitemap.default_host = "https://#{Spree::Store.default.url}"
+end
+
+##
+## If using Heroku or similar service where you want sitemaps to live in S3 you'll need to setup these settings.
+##
+
+## Pick a place safe to write the files
+# SitemapGenerator::Sitemap.public_path = 'tmp/'
+
+## Store on S3 using Fog - Note must add fog to your Gemfile.
+# SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(aws_access_key_id:     Spree::Config[:s3_access_key],
+#                                                                     aws_secret_access_key: Spree::Config[:s3_secret],
+#                                                                     fog_provider:          'AWS',
+#                                                                     fog_directory:         Spree::Config[:s3_bucket])
+
+## Inform the map cross-linking where to find the other maps.
+# SitemapGenerator::Sitemap.sitemaps_host = "http://#{Spree::Config[:s3_bucket]}.s3.amazonaws.com/"
+
+## Pick a namespace within your bucket to organize your maps. Note you'll need to set this directory to be public.
+# SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+
+# to generate the sitemap: rake sitemap:refresh
+
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options = {})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: priority: 0.5, changefreq: 'weekly',
+  #           lastmod: Time.now, host: default_host
+  #
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, priority: 0.7, changefreq: 'daily'
+  #
+  # Add individual articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), lastmod: article.updated_at
+  #   end
+  add_login
+  add_signup
+  add_account
+  add_password_reset
+  add_taxons
+  add_products
+end
