4a062262f140abef06ff0d67516b9ac6aa797ce6
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-08-01 17:58:52 +0200

    update default store email and description

--- db/default/spree/stores.rb
+++ db/default/spree/stores.rb
@@ -4,8 +4,8 @@ unless Spree::Store.where(code: 'spree').exists?
     s.code              = 'OctoBot Community'
     s.name              = 'OctoBot Community'
     s.url               = ENV['HOST_DOMAIN'] || 'community.octobot.online'
-    s.mail_from_address = 'drakkar.software@octobot.online'
-    s.meta_description  = "Simply customize and improve your OctoBot using the community extensions. Test and use new strategies, additional features and more."
+    s.mail_from_address = ENV['EMAIL_USERNAME'] || 'do-not-reply@octobot.online'
+    s.meta_description  = "Simply customize and improve your OctoBot using the community profiles and extensions. Test and use new setups, strategies, additional features and more."
     s.seo_title         = "OctoBot Community"
     s.meta_keywords     = "octobot community, octobot, community, trading, open source, community, cryptocurrency, cryptocurrencies, bitcoin, ethereum"
     s.default_currency  = 'USD'
