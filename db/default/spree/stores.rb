f4795a1d51a9f0f5c0831dc20a9810d166ed572e
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-08-01 21:01:26 +0200

    add default support email

--- db/default/spree/stores.rb
+++ db/default/spree/stores.rb
@@ -5,6 +5,7 @@ unless Spree::Store.where(code: 'spree').exists?
     s.name              = 'OctoBot Community'
     s.url               = ENV['HOST_DOMAIN'] || 'community.octobot.online'
     s.mail_from_address = ENV['EMAIL_USERNAME'] || 'do-not-reply@octobot.online'
+    s.customer_support_email = ENV['SUPPORT_EMAIL'] || 'drakkar.software@octobot.online'
     s.meta_description  = "Simply customize and improve your OctoBot using the community profiles and extensions. Test and use new setups, strategies, additional features and more."
     s.seo_title         = "OctoBot Community"
     s.meta_keywords     = "octobot community, octobot, community, trading, open source, community, cryptocurrency, cryptocurrencies, bitcoin, ethereum"
