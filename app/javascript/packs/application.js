8e99b3bbe3764320bad32146159bc4099c6965c2
GuillaumeDSM <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-08-02 19:23:58 +0200

    use import in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 // Only add libs that are not managed by Sprockets (used by spree)
-require("@fortawesome/fontawesome-free/js/all");
+import "@fortawesome/fontawesome-free/js/all";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
