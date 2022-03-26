729b41e70b7476246bff00128aaa239eb35f9133
Herklos <herklos.dev@protonmail.com>
2022-03-26 18:19:13 +0100

    [Seeds] Add default taxonomies and taxons

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,18 @@
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
+# TODO replace deprecated Spree::Store.current
+current_store = Spree::Store.current
+
+# Create frontend mandatory taxonomies
+Spree::Taxonomy.create([{ name: "Categories", store: current_store },
+                        { name: "Brands", store: current_store }])
+
+# Create tentacle product taxon
+taxonomy = Spree::Taxonomy.find_by!(name: "Categories", store: current_store)
+Spree::Taxon.find_or_create_by!(name: "Tentacle", taxonomy: taxonomy, parent: taxonomy.root)
+
+
 # Drakkar-Software's engine seeds
 SpreeCloud::Engine.load_seed if defined?(SpreeCloud)
 SpreeDonations::Engine.load_seed if defined?(SpreeDonations)
-
-# Create frontend mandatory taxonomies
-Spree::Taxonomy.create([{ name: "Categories", store: Spree::Store.current },
-                        { name: "Brands", store: Spree::Store.current }])
