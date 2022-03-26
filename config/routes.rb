e993d4cc981d470c8bb266d38bfe43810fe92e2e
Herklos <herklos.dev@protonmail.com>
2022-03-26 18:17:35 +0100

    [Routes] Disable sidekiq authentication for now

--- config/routes.rb
+++ config/routes.rb
@@ -11,24 +11,20 @@ Rails.application.routes.draw do
 
   root to: redirect('/404') # redirect / instead of rails welcome
 
-  if Rails.env.production?
-    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      # Protect against timing attacks:
-      # - See https://codahale.com/a-lesson-in-timing-attacks/
-      # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
-      # - Use & (do not use &&) so that it doesn't short circuit.
-      # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
-    end
-  end
+  # if Rails.env.production?
+  #   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  #     # Protect against timing attacks:
+  #     # - See https://codahale.com/a-lesson-in-timing-attacks/
+  #     # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+  #     # - Use & (do not use &&) so that it doesn't short circuit.
+  #     # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+  #     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+  #       ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+  #   end
+  # end
 
   # sidekiq web UI
   require 'sidekiq/web'
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    username == Rails.application.secrets.sidekiq_username &&
-      password == Rails.application.secrets.sidekiq_password
-  end
   mount Sidekiq::Web, at: '/admin/sidekiq'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
