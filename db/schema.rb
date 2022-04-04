3f624a3896201564cfd76f845644f6daafde4e0e
Herklos <herklos.dev@protonmail.com>
2022-04-04 22:24:30 +0200

    Add signals to db
    Signed-off-by: Herklos <herklos.dev@protonmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_03_212034) do
+ActiveRecord::Schema.define(version: 2022_04_04_202035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1248,6 +1248,18 @@ ActiveRecord::Schema.define(version: 2022_04_03_212034) do
     t.index ["shipment_id"], name: "index_spree_shipping_rates_on_shipment_id"
     t.index ["shipping_method_id"], name: "index_spree_shipping_rates_on_shipping_method_id"
     t.index ["tax_rate_id"], name: "index_spree_shipping_rates_on_tax_rate_id"
+  end
+
+  create_table "spree_signals", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "user_id"
+    t.string "uuid"
+    t.text "value"
+    t.string "version"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_spree_signals_on_product_id"
+    t.index ["user_id"], name: "index_spree_signals_on_user_id"
   end
 
   create_table "spree_state_changes", id: :serial, force: :cascade do |t|
