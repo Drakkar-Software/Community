14a6fa7ea906e5844a091a7d91a48333f5c6f3c4
Herklos <herklos.dev@protonmail.com>
2022-04-18 15:19:46 +0200

    [Terraform] Refactor alb
    Signed-off-by: Herklos <herklos.dev@protonmail.com>

--- terraform/beanstalk/application.tf
+++ terraform/beanstalk/application.tf
@@ -2,6 +2,7 @@ module "elastic_beanstalk_application" {
   source  = "cloudposse/elastic-beanstalk-application/aws"
   version = "0.11.1"
 
+  name        = "${var.name}-${var.stage}"
   description = var.description
 
   context = module.this.context
