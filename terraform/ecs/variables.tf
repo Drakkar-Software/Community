28acf4f7ce0670fae33e1b26469511e29285f9a9
Herklos <herklos.dev@protonmail.com>
2022-04-23 17:02:05 +0200

    [Terraform] Refactor ecs

--- terraform/ecs/variables.tf
+++ terraform/ecs/variables.tf
@@ -1,17 +1,350 @@
 variable "region" {
   type        = string
   description = "AWS Region"
-  default     = "us-east-2"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "VPC CIDR block"
 }
 
 variable "certificate_arn" {
   type        = string
   description = "SSL certificate ARN for ALB HTTPS endpoints"
-  default     = "arn:aws:acm:us-east-2:007561943850:certificate/9a1b8788-a0b0-4dc7-82e4-743267db404c"
 }
 
-variable "default_container_image" {
+variable "dns_zone_id" {
   type        = string
-  description = "The default container image to use in container definition"
-  default     = "nginxdemos/hello:latest"
+  description = "Route53 parent zone ID. The module will create sub-domain DNS record in the parent zone for the EB environment"
+}
+
+# cache
+variable "cache_cluster_size" {
+  type        = number
+  description = "Number of nodes in cluster"
+}
+
+variable "cache_instance_type" {
+  type        = string
+  description = "Elastic cache instance type"
+}
+
+variable "cache_family" {
+  type        = string
+  description = "Redis family"
+}
+
+variable "cache_engine_version" {
+  type        = string
+  description = "Redis engine version"
+}
+
+# db
+variable "database_name" {
+  type        = string
+  description = "The name of the database to create when the DB instance is created"
+}
+
+variable "database_user" {
+  type        = string
+  description = "Username for the master DB user"
+}
+
+variable "database_password" {
+  type        = string
+  description = "Password for the master DB user"
+}
+
+variable "database_port" {
+  type        = number
+  description = "Database port (_e.g._ `3306` for `MySQL`). Used in the DB Security Group to allow access to the DB instance from the provided `security_group_ids`"
+}
+
+variable "database_deletion_protection" {
+  type        = bool
+  description = "Set to true to enable deletion protection on the RDS instance"
+}
+
+variable "database_multi_az" {
+  type        = bool
+  description = "Set to true if multi AZ deployment must be supported"
+}
+
+variable "database_availability_zone" {
+  type        = string
+  default     = null
+  description = "The AZ for the RDS instance. Specify one of `subnet_ids`, `db_subnet_group_name` or `availability_zone`. If `availability_zone` is provided, the instance will be placed into the default VPC or EC2 Classic"
+}
+
+variable "db_subnet_group_name" {
+  type        = string
+  default     = null
+  description = "Name of DB subnet group. DB instance will be created in the VPC associated with the DB subnet group. Specify one of `subnet_ids`, `db_subnet_group_name` or `availability_zone`"
+}
+
+variable "database_storage_type" {
+  type        = string
+  description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), or 'io1' (provisioned IOPS SSD)"
+}
+
+variable "database_storage_encrypted" {
+  type        = bool
+  description = "(Optional) Specifies whether the DB instance is encrypted. The default is false if not specified"
+}
+
+variable "database_allocated_storage" {
+  type        = number
+  description = "The allocated storage in GBs"
+}
+
+variable "database_engine" {
+  type        = string
+  description = "Database engine type"
+  # http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+  # - mysql
+  # - postgres
+  # - oracle-*
+  # - sqlserver-*
+}
+
+variable "database_engine_version" {
+  type        = string
+  description = "Database engine version, depends on engine type"
+  # http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+}
+
+variable "database_major_engine_version" {
+  type        = string
+  description = "Database MAJOR engine version, depends on engine type"
+  # https://docs.aws.amazon.com/cli/latest/reference/rds/create-option-group.html
+}
+
+variable "database_instance_class" {
+  type        = string
+  description = "Class of RDS instance"
+  # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+}
+
+variable "db_parameter_group" {
+  type        = string
+  description = "Parameter group, depends on DB engine used"
+  # "mysql5.6"
+  # "postgres9.5"
+}
+
+variable "database_publicly_accessible" {
+  type        = bool
+  description = "Determines if database can be publicly available (NOT recommended)"
+}
+
+variable "database_apply_immediately" {
+  type        = bool
+  description = "Specifies whether any database modifications are applied immediately, or during the next maintenance window"
+}
+
+# app
+variable "app_secret_key_base" {
+  type        = string
+  description = "App secret key base"
+}
+
+variable "rails_max_thread" {
+  type        = string
+  description = "Rails max thread count"
+}
+
+variable "rails_env" {
+  type        = string
+  description = "Rails env (development, production...)"
+}
+
+# container
+variable "container_name" {
+  type        = string
+  description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)"
+}
+
+variable "container_image" {
+  type        = string
+  description = "The image used to start the container. Images in the Docker Hub registry available by default"
+}
+
+variable "container_memory" {
+  type        = number
+  description = "The amount of memory (in MiB) to allow the container to use. This is a hard limit, if the container attempts to exceed the container_memory, the container is killed. This field is optional for Fargate launch type and the total amount of container_memory of all containers in a task will need to be lower than the task memory value"
+  default     = null
+}
+
+variable "container_memory_reservation" {
+  type        = number
+  description = "The amount of memory (in MiB) to reserve for the container. If container needs to exceed this threshold, it can do so up to the set container_memory hard limit"
+  default     = null
+}
+
+variable "container_definition" {
+  type        = map
+  description = "Container definition overrides which allows for extra keys or overriding existing keys."
+  default     = {}
+}
+
+variable "container_port_mappings" {
+  type = list(object({
+    containerPort = number
+    hostPort      = number
+    protocol      = string
+  }))
+
+  description = "The port mappings to configure for the container. This is a list of maps. Each map should contain \"containerPort\", \"hostPort\", and \"protocol\", where \"protocol\" is one of \"tcp\" or \"udp\". If using containers in a task with the awsvpc or host network mode, the hostPort can either be left blank or set to the same value as the containerPort"
+
+  default = []
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_HealthCheck.html
+variable "container_healthcheck" {
+  type = object({
+    command     = list(string)
+    retries     = number
+    timeout     = number
+    interval    = number
+    startPeriod = number
+  })
+  description = "A map containing command (string), timeout, interval (duration in seconds), retries (1-10, number of times to retry before marking container unhealthy), and startPeriod (0-300, optional grace period to wait, in seconds, before failed healthchecks count toward retries)"
+  default     = null
+}
+
+variable "container_cpu" {
+  type        = number
+  description = "The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
+  default     = 0
+}
+
+variable "container_essential" {
+  type        = bool
+  description = "Determines whether all other containers in a task are stopped, if this container fails or stops for any reason. Due to how Terraform type casts booleans in json it is required to double quote this value"
+  default     = true
+}
+
+variable "container_entrypoint" {
+  type        = list(string)
+  description = "The entry point that is passed to the container"
+  default     = null
+}
+
+variable "container_command" {
+  type        = list(string)
+  description = "The command that is passed to the container"
+  default     = null
+}
+
+variable "container_working_directory" {
+  type        = string
+  description = "The working directory to run commands inside the container"
+  default     = null
+}
+
+variable "container_environment" {
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  description = "The environment variables to pass to the container. This is a list of maps. map_environment overrides environment"
+  default     = []
+}
+
+variable "container_extra_hosts" {
+  type = list(object({
+    ipAddress = string
+    hostname  = string
+  }))
+  description = "A list of hostnames and IP address mappings to append to the /etc/hosts file on the container. This is a list of maps"
+  default     = null
+}
+
+variable "container_map_environment" {
+  type        = map(string)
+  description = "The environment variables to pass to the container. This is a map of string: {key: value}. map_environment overrides environment"
+  default     = null
+}
+
+variable "container_secrets" {
+  type = list(object({
+    name      = string
+    valueFrom = string
+  }))
+  description = "The secrets to pass to the container. This is a list of maps"
+  default     = null
+}
+
+variable "container_readonly_root_filesystem" {
+  type        = bool
+  description = "Determines whether a container is given read-only access to its root filesystem. Due to how Terraform type casts booleans in json it is required to double quote this value"
+  default     = false
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html
+variable "container_log_configuration" {
+  type        = any
+  description = "Log configuration options to send to a custom log driver for the container. For more details, see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html"
+  default     = null
+}
+
+# service task
+variable "ecs_launch_type" {
+  type        = string
+  description = "ECS launch type"
+}
+
+variable "network_mode" {
+  type        = string
+  description = "The network mode to use for the task. This is required to be `awsvpc` for `FARGATE` `launch_type`"
+}
+
+variable "task_cpu" {
+  type        = number
+  description = "The number of CPU units used by the task. If using `FARGATE` launch type `task_cpu` must match supported memory values (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)"
+}
+
+variable "task_memory" {
+  type        = number
+  description = "The amount of memory (in MiB) used by the task. If using Fargate launch type `task_memory` must match supported cpu value (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)"
+}
+
+variable "desired_count" {
+  type        = number
+  description = "The number of instances of the task definition to place and keep running"
+}
+
+variable "deployment_controller_type" {
+  type        = string
+  description = "Type of deployment controller. Valid values are `CODE_DEPLOY` and `ECS`"
+}
+
+variable "deployment_maximum_percent" {
+  type        = number
+  description = "The upper limit of the number of tasks (as a percentage of `desired_count`) that can be running in a service during a deployment"
+}
+
+variable "deployment_minimum_healthy_percent" {
+  type        = number
+  description = "The lower limit (as a percentage of `desired_count`) of the number of tasks that must remain running and healthy in a service during a deployment"
+}
+
+variable "ignore_changes_task_definition" {
+  type        = bool
+  description = "Whether to ignore changes in container definition and task definition in the ECS service"
+}
+
+variable "assign_public_ip" {
+  type        = bool
+  description = "Assign a public IP address to the ENI (Fargate launch type only). Valid values are `true` or `false`. Default `false`"
+}
+
+variable "propagate_tags" {
+  type        = string
+  description = "Specifies whether to propagate the tags from the task definition or the service to the tasks. The valid values are SERVICE and TASK_DEFINITION"
 }
