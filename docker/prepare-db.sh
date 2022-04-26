ca595735e7be3e698bad225028046c8371cfe13c
Herklos <herklos.dev@protonmail.com>
2022-04-26 14:33:51 +0200

    [CI]Add okteto deploy

--- docker/prepare-db.sh
+++ docker/prepare-db.sh
@@ -1,5 +1,11 @@
-
 #! /bin/sh
 
-# If database exists, migrate. Otherweise setup (create and seed)
-bundle exec rake db:prepare && echo "Database is ready!"
+if bundle exec rails db:exists ; then
+  echo "Database already exists, migrating..."
+  bundle exec rails db:migrate
+else
+  echo "Database doesn't exist, creating..."
+  bundle exec rails db:setup
+fi
+
+echo "Database is ready!"
