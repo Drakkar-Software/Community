98073cc623eb0c5d2429cde0183ccd2cd7aa3537
Herklos <herklos.dev@protonmail.com>
2022-04-26 21:37:53 +0200

    [Terraform] Remove for now

--- terraform/beanstalk/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-bundle exec rake db:prepare && echo "Database is ready!"
-bundle exec rails s -b 0.0.0.0 -p 80
--- terraform/ecs/vpc.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  source  = "cloudposse/vpc/aws"
-  version = "0.18.0"
-
-  cidr_block = var.vpc_cidr_block
-
-  context = module.this.context
-}
