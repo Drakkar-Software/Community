ee00b6ea773fd0155cc60d8990f5c1672b2c2758
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-04-26 23:37:21 +0200

    update docker/prepare-db.sh

--- docker/prepare-db.sh
+++ docker/prepare-db.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
-
-if bundle exec rails db:exists ; then
-  echo "Database already exists, migrating..."
-  bundle exec rails db:migrate
-else
-  echo "Database doesn't exist, creating..."
-  bundle exec rake db:create
-  echo "Database migrate..."
-  bundle exec rake db:migrate
-  echo "Database seed..."
-  bundle exec rake db:seed
-fi
+bundle exec rake db:prepare && echo "prepare ok"
+#if bundle exec rails db:exists ; then
+#  echo "Database already exists, migrating..."
+#  bundle exec rails db:migrate
+#else
+#  echo "Database doesn't exist, creating..."
+#  bundle exec rake db:create
+#  echo "Database migrate..."
+#  bundle exec rake db:migrate
+#  echo "Database seed..."
+#  bundle exec rake db:seed
+#fi
 
 echo "Database is ready!"
