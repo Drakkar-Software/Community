e3a3d6c9510c5f0a8909457a0a6636d5c4144bd5
Herklos <herklos.dev@protonmail.com>
2022-04-26 21:37:53 +0200

    [Terraform] Remove for now

--- terraform/beanstalk/application.tf
+++
@@ -1,9 +0,0 @@
-module "elastic_beanstalk_application" {
-  source  = "cloudposse/elastic-beanstalk-application/aws"
-  version = "0.11.1"
-
-  name        = var.name
-  description = var.description
-
-  context = module.this.context
-}
