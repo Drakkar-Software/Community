2a4f8a33e4a4dc46fdc385f47aa125a9b6a43ad9
Herklos <herklos.dev@protonmail.com>
2022-04-28 15:15:44 +0200

    Temporary don't use puma

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,4 +2,6 @@
 
 ./docker/wait-for-services.sh
 ./docker/prepare-db.sh
-bundle exec puma -C config/puma.rb
+
+# bundle exec puma -C config/puma.rb TODO should run this command
+bundle exec rails s -b 0.0.0.0 # TODO remove this one
