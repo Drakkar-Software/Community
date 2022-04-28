21fdba66ab93a1d5bbe24dd31b4bc15af1740abb
Herklos <herklos.dev@protonmail.com>
2022-04-28 15:15:44 +0200

    Revert "Temporary don't use puma"
    This reverts commit 2a4f8a33e4a4dc46fdc385f47aa125a9b6a43ad9.

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,6 +2,4 @@
 
 ./docker/wait-for-services.sh
 ./docker/prepare-db.sh
-
-# bundle exec puma -C config/puma.rb TODO should run this command
-bundle exec rails s -b 0.0.0.0 # TODO remove this one
+bundle exec puma -C config/puma.rb
