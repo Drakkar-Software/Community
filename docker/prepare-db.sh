4999145f86c6b2474dad0d204e2581d97610a64b
Herklos <herklos.dev@protonmail.com>
2022-06-16 22:39:20 +0200

    [Docker][PrepareDB] Restore db:prepare usage

--- docker/prepare-db.sh
+++ docker/prepare-db.sh
@@ -1,11 +1,4 @@
 #! /bin/sh
 
-if bundle exec rails db:exists ; then
-  echo "Database already exists, migrating..."
-  bundle exec rails db:migrate
-else
-  echo "Database doesn't exist, creating..."
-  bundle exec rails db:setup
-fi
-
-echo "Database is ready!"
+# If database exists, migrate. Otherweise setup (create and seed)
+bundle exec rake db:prepare && echo "Database is ready!"
