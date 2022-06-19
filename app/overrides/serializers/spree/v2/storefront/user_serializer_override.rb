bc54579b5c052c6da564b283fb214d48e8a2ae58
Herklos <herklos.dev@protonmail.com>
2022-06-19 22:23:57 +0200

    [UserSerializer] Use product.slug instead of product.id

--- app/overrides/serializers/spree/v2/storefront/user_serializer_override.rb
+++ app/overrides/serializers/spree/v2/storefront/user_serializer_override.rb
@@ -1,5 +1,5 @@
 Spree::V2::Storefront::UserSerializer.class_eval do
   attribute :authored_products do |object|
-    object.authored_products.pluck(:id)
+    object.authored_products.pluck(:slug)
   end
 end
